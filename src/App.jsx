fc1469b3e27d8532572af92c22518f75a7733a4b
muhamedsafwat <musafwat666@gmail.com>
2023-10-25 22:46:08 +0300

    files upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { HeroSection, Partners, Contact } from "./components/sections";
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <Partners />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
